1c085802c507ff24edddf0575c3c8bd289bda2e8
Bryan McNamara <116223024@umail.ucc.ie>
2020-05-24 20:18:49 +0100

    Added Game Window and dymanic rendering of enemy objects.

---
+++ BlazeInvaders/Shared/EnemyComponentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazeInvaders.Shared
+{
+    public class EnemyComponentModel
+    {
+        
+        public int X { get; set; }
+        
+        public int Y { get; set; }
+        
+        public string enemyType { get; set; }
+    }
+}
